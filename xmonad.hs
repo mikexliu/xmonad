2d1e370abcd3a9068cbbbb5cff8b21f454e5be57
Mike Liu <me@mikeliu.io>
2014-07-06 15:30:03 -0700

    clean up from clone

--- xmonad.hs
+++ xmonad.hs
@@ -2,9 +2,6 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
-import XMonad.Hooks.SetWMName
-import System.IO
 
 myTerminal = "gnome-terminal"
 
@@ -14,9 +11,5 @@ xmonad $ defaultConfig
     {
       terminal = myTerminal
     , manageHook = manageDocks <+> manageHook defaultConfig
-    , startupHook = setWMName "LG3D"
     , layoutHook = avoidStruts  $  layoutHook defaultConfig
-    } `additionalKeys`
-    [ ((mod4Mask .|. shiftMask, xK_z), spawn "gnome-screensaver-command --lock") --mod4mask is the windows key
-    , ((0, xK_Print), spawn "gnome-screenshot")
-    ]
+    }
